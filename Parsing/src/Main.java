4616fab04afb6249594128b1996ea2f37cf8c5a6
belyaeval <belen@yandex.ru>
2020-05-31 15:45:33 +0700

    Добавлена реализация метода parseTarget()

--- Parsing/src/Main.java
+++ Parsing/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
+        HashMap<String, Integer> paths = new HashMap<>();
+        String source = "https://ru.wikipedia.org/wiki/%D0%91%D0%BE%D0%B9%D0%BB,_%D0%94%D1%8D%D0%BD%D0%BD%D0%B8";
+        String target = "https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D1%8F";
 
+        WebParser webParser = new WebParser(source, target, paths);
+
+
+        paths = webParser.parseTarget(new HashSet<>());
+        webParser.parseSource(1, new HashSet<>(), new ArrayList<>());
+
+        System.out.println(paths.get(source));
     }
 }
--- Parsing/src/WebParser.java
+++ Parsing/src/WebParser.java
@@ -8,10 +8,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class WebParser {
+class WebParser {
     private String source;
     private String target;
     private Document sourceHTML;
+    private Document targetHTML;
     private Document internalHTML;
     private HashMap<String, Integer> paths;
 
@@ -21,7 +22,7 @@ public class WebParser {
         this.paths = paths;
     }
 
-    public void parseSource(int j, HashSet<String> links, ArrayList<String> visited) {
+    void parseSource(int j, HashSet<String> links, ArrayList<String> visited) {
         try {
             sourceHTML = Jsoup.connect(source).get();
         } catch (IOException e) {
@@ -110,7 +111,67 @@ public class WebParser {
         parseSource(j, links, visited);
     }
 
-    public void parseTarget() {
+    HashMap<String, Integer> parseTarget(HashSet<String> links) {
+        try {
+            targetHTML = Jsoup.connect(source).get();
+        } catch (IOException e) {
+            e.getMessage();
+        }
 
+        Elements content = targetHTML.select("div.mw-parser-output").select("a[href*=/wiki/]").not("[class=image]");
+
+        for (Element elem : content) {
+            if (!elem.text().equals("") && elem.attr("abs:href").contains("ru.wikipedia.org") && !elem.attr("class").contains("mw-magiclink-isbn")) {
+                links.add(elem.attr("abs:href"));
+            }
+        }
+
+        int i = 1;
+
+        while (true) {
+            for (String link : links) {
+                try {
+                    internalHTML = Jsoup.connect(link).userAgent("Chrome/83.0.4103.61").get();
+                } catch (IOException e) {
+                    e.getMessage();
+                }
+
+                Elements internalContent = internalHTML.select("div.mw-parser-output").select("a[href*=/wiki/]").not("[class=image]");
+
+                for (Element elem : internalContent) {
+                    if (!elem.text().equals("") && elem.attr("abs:href").contains("ru.wikipedia.org") && !elem.attr("class").contains("mw-magiclink-isbn")) {
+                        boolean isExist = false;
+                        for (String internalLink : paths.keySet()) {
+                            if (i == 1) {
+                                if (elem.attr("abs:href").equals(target)) {
+                                    isExist = true;
+                                    break;
+                                }
+                            } else {
+                                if (elem.attr("abs:href").equals(internalLink)) {
+                                    isExist = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (isExist) {
+                            paths.put(link, i);
+                            break;
+
+                        }
+                    }
+                }
+            }
+            int sizeBefore = links.size();
+
+            links.removeAll(paths.keySet());
+            if (sizeBefore == links.size()) {
+                break;
+            }
+
+            i++;
+        }
+
+        return paths;
     }
 }
